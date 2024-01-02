1201d701c968d2592fe79f1c7d0b9d9017f6c472
BrunoBafilli <144603933+lognsoft@users.noreply.github.com>
2024-01-02 09:14:51 -0300

    Materiais
    Materiais

---
+++ rndGrafica/CriandoMeuPrimeiroProjeto/meuprimeiroprojeto/WinFormsApp8/WinFormsApp8/Form1.cs
@@ -0,0 +1,22 @@
+namespace WinFormsApp8
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCadastrar_Click(object sender, EventArgs e)
+        {
+            string produto = txtProduto.Text;
+
+            
+        }
+
+        private void btnCadastrar_Click_2(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
