8e066aecc192ca5750d59146a627ab501baddc3f
BrunoBafilli <144603933+lognsoft@users.noreply.github.com>
2024-01-02 09:13:20 -0300

    Material
    Material

--- Modulo-02/Aula-08/Program.cs
+++
@@ -1,8 +0,0 @@
-﻿Console.WriteLine("Digite um valor a ser somado");
-int numero1 = Convert.ToInt16(Console.ReadLine());
-
-Console.WriteLine("Digite o segundo valor a ser somado");
-int numero2 = Convert.ToInt16(Console.ReadLine());
-int totalSoma = numero1 + numero2;
-
-Console.WriteLine("A soma entre o numero: {0} e {1} = {2}", numero1, numero2, totalSoma);
--- Modulo-02/Aula-10/Program.cs
+++
@@ -1,6 +0,0 @@
-﻿int numero = Convert.ToInt32(Console.ReadLine());
-
-for (int i = 0; i < numero; i++)
-{
-    Console.WriteLine(i * 4);
-}
